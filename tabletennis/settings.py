bd4897e79bee3aea0669d1495f1881c16f1e49c4
SeRyuu25 <joshiryus@gmail.com>
2025-05-21 14:44:18 +0700

    update settings.py for deploy

--- tabletennis/settings.py
+++ tabletennis/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,7 +155,6 @@ STATICFILES_DIRS = [
 
 
 # Email Settings
-from decouple import config
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'in-v3.mailjet.com'
@@ -189,7 +190,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Messages
-from django.contrib.messages import constants as message_constants
 
 MESSAGE_TAGS = {
     message_constants.DEBUG:   "secondary",
